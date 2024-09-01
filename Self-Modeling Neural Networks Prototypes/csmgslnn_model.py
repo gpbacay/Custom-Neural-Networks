b5c26825adb924d9fe5c2987bb9733d6367dc5da
gpbacay <giannebacay2004@gmail.com>
2024-09-01 20:29:38 +0800

    updated CSMGSLNN

--- Self-Modeling Neural Networks Prototypes/csmgslnn_model.py
+++ Self-Modeling Neural Networks Prototypes/csmgslnn_model.py
@@ -1,4 +1,4 @@
-# model.py
+import numpy as np
 import tensorflow as tf
 from tensorflow.keras.layers import Dense, Input, Lambda, Dropout, Flatten, Conv2D, MaxPooling2D
 from tensorflow.keras import Model
