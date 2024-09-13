db4c093326eea4d0907be3ff0970e304c0da97c2
gpbacay <giannebacay2004@gmail.com>
2024-09-13 10:46:30 +0800

    ...

--- Self-Modeling Neural Networks Prototypes/CSMSELNN/csmselnn_mnist_v2.py
+++ Self-Modeling Neural Networks Prototypes/CSMSELNN/csmselnn_mnist_v2.py
@@ -6,7 +6,7 @@ from tensorflow.keras.models import Model
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 
-class SynaptogenesisLayer(tf.keras.layers.Layer):
+class ReservoirComputingLayer(tf.keras.layers.Layer):
     def __init__(self, initial_reservoir_size, input_dim, spectral_radius, leak_rate, spike_threshold, max_reservoir_dim, **kwargs):
         super().__init__(**kwargs)
         self.initial_reservoir_size = initial_reservoir_size
@@ -20,42 +20,43 @@ class SynaptogenesisLayer(tf.keras.layers.Layer):
         self.refractory_period = 5
         self.state_size = max_reservoir_dim
         self.output_size = max_reservoir_dim
+        self.current_size = initial_reservoir_size
 
     def build(self, input_shape):
-        self.initialize_weights()
+        self._initialize_weights()
         super().build(input_shape)
 
-    def initialize_weights(self):
+    def _initialize_weights(self):
         reservoir_weights = np.random.randn(self.initial_reservoir_size, self.initial_reservoir_size) * 0.1
         reservoir_weights = np.dot(reservoir_weights, reservoir_weights.T)
         spectral_radius = np.max(np.abs(np.linalg.eigvals(reservoir_weights)))
         reservoir_weights *= self.spectral_radius / spectral_radius
         self.reservoir_weights = self.add_weight(
             name='reservoir_weights',
-            shape=(self.initial_reservoir_size, self.initial_reservoir_size),
-            initializer=tf.constant_initializer(reservoir_weights),
+            shape=(self.max_reservoir_dim, self.max_reservoir_dim),
+            initializer=tf.constant_initializer(np.pad(reservoir_weights, ((0, self.max_reservoir_dim - self.initial_reservoir_size), (0, self.max_reservoir_dim - self.initial_reservoir_size)))),
             trainable=False
         )
 
         input_weights = np.random.randn(self.initial_reservoir_size, self.input_dim) * 0.1
         self.input_weights = self.add_weight(
             name='input_weights',
-            shape=(self.initial_reservoir_size, self.input_dim),
-            initializer=tf.constant_initializer(input_weights),
+            shape=(self.max_reservoir_dim, self.input_dim),
+            initializer=tf.constant_initializer(np.pad(input_weights, ((0, self.max_reservoir_dim - self.initial_reservoir_size), (0, 0)))),
             trainable=False
         )
 
     def call(self, inputs, states):
-        prev_state = states[0][:, :tf.shape(self.reservoir_weights)[0]]
-        input_contribution = tf.matmul(inputs, self.input_weights, transpose_b=True)
-        reservoir_contribution = tf.matmul(prev_state, self.reservoir_weights)
+        prev_state = states[0][:, :self.current_size]
+        input_contribution = tf.matmul(inputs, tf.transpose(self.input_weights[:self.current_size]))
+        reservoir_contribution = tf.matmul(prev_state, self.reservoir_weights[:self.current_size, :self.current_size])
 
         state = (1 - self.leak_rate) * prev_state + self.leak_rate * tf.tanh(input_contribution + reservoir_contribution)
         spikes = tf.cast(tf.greater(state, self.spike_threshold), dtype=tf.float32)
         state = tf.where(spikes > 0, state - self.spike_threshold, state)
         refractory_mask = tf.reduce_sum(spikes, axis=1) > self.refractory_period
         state = tf.where(tf.expand_dims(refractory_mask, 1), tf.zeros_like(state), state)
-        padded_state = tf.pad(state, [[0, 0], [0, self.max_reservoir_dim - tf.shape(state)[-1]]])
+        padded_state = tf.pad(state, [[0, 0], [0, self.max_reservoir_dim - tf.shape(state)[1]]])
 
         return padded_state, [padded_state]
 
@@ -64,61 +65,75 @@ class SynaptogenesisLayer(tf.keras.layers.Layer):
             dtype = tf.float32
         return [tf.zeros((batch_size, self.max_reservoir_dim), dtype=dtype)]
 
-    def add_synapses(self):
-        current_size = tf.shape(self.reservoir_weights)[0]
-        growth_rate = tf.maximum(1, tf.cast(tf.math.floor(tf.cast(current_size, tf.float32) * 0.1), tf.int32))
-        new_neurons = tf.minimum(growth_rate, self.max_reservoir_dim - current_size)
+    def _expand_reservoir(self):
+        growth_rate = tf.maximum(1, tf.cast(tf.math.floor(tf.cast(self.current_size, tf.float32) * 0.1), tf.int32))
+        new_neurons = tf.minimum(growth_rate, self.max_reservoir_dim - self.current_size)
         
         if new_neurons <= 0:
             return  # No room to grow
         
-        new_size = current_size + new_neurons
+        new_size = self.current_size + new_neurons
         
-        # Create the new part of the weight matrix (for new synapses)
-        new_reservoir_weights = tf.random.normal((new_neurons, new_neurons)) * 0.1
-        new_input_weights = tf.random.normal((new_neurons, self.input_dim)) * 0.1
+        # Create new weights for the expanded part
+        new_weights = tf.random.normal((new_neurons, new_size)) * 0.1
+        new_weights = tf.concat([tf.zeros((new_neurons, self.current_size)), new_weights[:, self.current_size:]], axis=1)
+        
+        # Update reservoir weights
+        updated_weights = tf.concat([
+            self.reservoir_weights[:self.current_size, :self.current_size],
+            tf.random.normal((self.current_size, new_neurons)) * 0.1
+        ], axis=1)
+        updated_weights = tf.concat([updated_weights, new_weights], axis=0)
+        
+        # Ensure symmetry
+        updated_weights = (updated_weights + tf.transpose(updated_weights)) / 2
+        
+        # Adjust spectral radius
+        spectral_radius = tf.math.real(tf.reduce_max(tf.abs(tf.linalg.eigvals(updated_weights))))
+        scaling_factor = self.spectral_radius / spectral_radius
+        updated_weights *= scaling_factor
 
-        # Expand the existing reservoir and input weight matrices
-        updated_reservoir_weights = tf.concat([
-            tf.concat([self.reservoir_weights, tf.zeros((current_size, new_neurons))], axis=1),
-            tf.concat([tf.zeros((new_neurons, current_size)), new_reservoir_weights], axis=1)
+        # Update input weights
+        new_input_weights = tf.concat([
+            self.input_weights[:self.current_size],
+            tf.random.normal((new_neurons, self.input_dim)) * 0.1
         ], axis=0)
 
-        # Scale the updated reservoir weights based on spectral radius
-        spectral_radius = tf.math.real(tf.reduce_max(tf.abs(tf.linalg.eigvals(updated_reservoir_weights))))
-        scaling_factor = self.spectral_radius / spectral_radius
-        updated_reservoir_weights *= scaling_factor
+        # Assign new weights
+        self.reservoir_weights.assign(tf.pad(updated_weights, [[0, self.max_reservoir_dim - new_size], [0, self.max_reservoir_dim - new_size]]))
+        self.input_weights.assign(tf.pad(new_input_weights, [[0, self.max_reservoir_dim - new_size], [0, 0]]))
+        self.current_size = new_size
 
-        # Concatenate the new input weights to the existing ones
-        updated_input_weights = tf.concat([self.input_weights, new_input_weights], axis=0)
+    def _prune_reservoir(self):
+        if self.current_size <= self.initial_reservoir_size:
+            return  # Don't prune below the initial size
 
-        # Now update the weight variables in place
-        self.reservoir_weights.assign(updated_reservoir_weights[:self.max_reservoir_dim, :self.max_reservoir_dim])
-        self.input_weights.assign(updated_input_weights[:self.max_reservoir_dim, :self.input_dim])
+        activity = tf.reduce_mean(tf.abs(self.reservoir_weights[:self.current_size, :self.current_size]), axis=0)
+        k = tf.cast(tf.cast(self.current_size, tf.float32) * 0.9, tf.int32)  # Ensure k is an integer
+        _, indices = tf.nn.top_k(activity, k=k)
+        indices = tf.sort(indices)
 
+        pruned_reservoir_weights = tf.gather(tf.gather(self.reservoir_weights[:self.current_size, :self.current_size], indices), indices, axis=1)
+        pruned_input_weights = tf.gather(self.input_weights[:self.current_size], indices)
 
-    def prune_synapses(self):
-        activity = tf.reduce_mean(tf.abs(self.reservoir_weights), axis=0)
-        threshold = np.percentile(activity.numpy(), 10)
-        mask = activity > threshold
-        pruned_weights = tf.where(tf.tile(mask[None, :], [tf.shape(self.reservoir_weights)[0], 1]), self.reservoir_weights, tf.zeros_like(self.reservoir_weights))
-        
-        # Update the existing weight variable instead of creating a new one
-        self.reservoir_weights.assign(pruned_weights)
+        new_size = tf.shape(pruned_reservoir_weights)[0]
+        self.reservoir_weights.assign(tf.pad(pruned_reservoir_weights, [[0, self.max_reservoir_dim - new_size], [0, self.max_reservoir_dim - new_size]]))
+        self.input_weights.assign(tf.pad(pruned_input_weights, [[0, self.max_reservoir_dim - new_size], [0, 0]]))
+        self.current_size = new_size
 
-class TemporalAttentionLayer(tf.keras.layers.Layer):
+class TemporalAttentionAggregator(tf.keras.layers.Layer):
     def call(self, inputs):
         weights = tf.reduce_mean(inputs, axis=1, keepdims=True)
         return tf.reduce_sum(inputs * weights, axis=1)
 
-class SelfModelingLayer(tf.keras.layers.Layer):
+class FeedbackModulationLayer(tf.keras.layers.Layer):
     def __init__(self, internal_units=128, feedback_strength=0.1, output_dense=4096, **kwargs):
         super().__init__(**kwargs)
         self.internal_units = internal_units
         self.feedback_strength = feedback_strength
         self.state_dense = Dense(internal_units, activation='relu')
         self.gate_dense = Dense(internal_units, activation='sigmoid')
-        self.output_dense = Dense(output_dense)  # Match the input dimension
+        self.output_dense = Dense(output_dense)
 
     def build(self, input_shape):
         self.feedback_weights = self.add_weight(
@@ -142,11 +157,11 @@ class SelfModelingLayer(tf.keras.layers.Layer):
         modulated_output = self.output_dense(modulated_internal)
         return modulated_output
 
-class SynaptogenesisCallback(Callback):
-    def __init__(self, synaptogenesis_layer, performance_metric='val_accuracy', target_metric=0.95,
+class DynamicReservoirGrowthCallback(Callback):
+    def __init__(self, reservoir_layer, performance_metric='val_accuracy', target_metric=0.95,
                  add_synapses_threshold=0.01, prune_synapses_threshold=0.1, growth_phase_length=10, pruning_phase_length=5):
         super().__init__()
-        self.synaptogenesis_layer = synaptogenesis_layer
+        self.reservoir_layer = reservoir_layer
         self.performance_metric = performance_metric
         self.target_metric = target_metric
         self.initial_add_synapses_threshold = add_synapses_threshold
@@ -175,17 +190,17 @@ class SynaptogenesisCallback(Callback):
         if len(self.performance_history) > 5:
             improvement_rate = (current_metric - self.performance_history[-5]) / 5
             if improvement_rate > 0.01:
-                self.synaptogenesis_layer.add_synapses()
+                self.reservoir_layer._expand_reservoir()
             elif improvement_rate < 0.001:
-                self.synaptogenesis_layer.prune_synapses()
+                self.reservoir_layer._prune_reservoir()
 
         if current_metric >= self.target_metric:
             if self.current_phase == 'growth' and current_metric < self.add_synapses_threshold:
-                self.synaptogenesis_layer.add_synapses()
+                self.reservoir_layer._expand_reservoir()
             elif self.current_phase == 'pruning' and current_metric > self.prune_synapses_threshold:
-                self.synaptogenesis_layer.prune_synapses()
+                self.reservoir_layer._prune_reservoir()
 
-def create_csmselnn_model(input_shape, initial_reservoir_size, spectral_radius, leak_rate, spike_threshold, max_reservoir_dim, output_dim):
+def create_reservoir_cnn_rnn_model(input_shape, initial_reservoir_size, spectral_radius, leak_rate, spike_threshold, max_reservoir_dim, output_dim):
     inputs = Input(shape=input_shape)
     x = Conv2D(32, (3, 3), activation='relu', padding='same')(inputs)
     x = MaxPooling2D((2, 2))(x)
@@ -196,10 +211,9 @@ def create_csmselnn_model(input_shape, initial_reservoir_size, spectral_radius, 
     x = Flatten()(x)
     x = Dropout(0.5)(x)
 
-    # Reshape the input for RNN layer
     x = Reshape((1, -1))(x)
 
-    synaptogenesis_layer = SynaptogenesisLayer(
+    reservoir_layer = ReservoirComputingLayer(
         initial_reservoir_size=initial_reservoir_size,
         input_dim=x.shape[-1],
         spectral_radius=spectral_radius,
@@ -207,13 +221,13 @@ def create_csmselnn_model(input_shape, initial_reservoir_size, spectral_radius, 
         spike_threshold=spike_threshold,
         max_reservoir_dim=max_reservoir_dim
     )
-    rnn_layer = RNN(synaptogenesis_layer, return_sequences=True)(x)
-    x = TemporalAttentionLayer()(rnn_layer)
-    x = SelfModelingLayer(internal_units=128, output_dense=max_reservoir_dim)(x)
+    rnn_layer = RNN(reservoir_layer, return_sequences=True)(x)
+    x = TemporalAttentionAggregator()(rnn_layer)
+    x = FeedbackModulationLayer(internal_units=128, output_dense=max_reservoir_dim)(x)
     outputs = Dense(output_dim, activation='softmax')(x)
 
     model = Model(inputs=inputs, outputs=outputs)
-    return model, synaptogenesis_layer
+    return model, reservoir_layer
 
 def preprocess_data(data):
     return data.astype('float32') / 255.0
@@ -235,7 +249,7 @@ def main():
     epochs = 10
     batch_size = 64
 
-    model, synaptogenesis_layer = create_csmselnn_model(
+    model, reservoir_layer = create_reservoir_cnn_rnn_model(
         input_shape=input_shape,
         initial_reservoir_size=initial_reservoir_size,
         spectral_radius=spectral_radius,
@@ -249,12 +263,12 @@ def main():
 
     early_stopping = EarlyStopping(patience=5, restore_best_weights=True)
     reduce_lr = ReduceLROnPlateau(factor=0.5, patience=3)
-    synaptogenesis_callback = SynaptogenesisCallback(synaptogenesis_layer)
+    dynamic_reservoir_growth_callback = DynamicReservoirGrowthCallback(reservoir_layer)
 
     history = model.fit(
         x_train, y_train, validation_data=(x_val, y_val),
         epochs=epochs, batch_size=batch_size,
-        callbacks=[early_stopping, reduce_lr, synaptogenesis_callback]
+        callbacks=[early_stopping, reduce_lr, dynamic_reservoir_growth_callback]
     )
 
     test_loss, test_acc = model.evaluate(x_test, y_test)
@@ -272,6 +286,7 @@ if __name__ == '__main__':
     main()
 
 
+
 # Convolutional Self-Modeling Spiking Elastic Liquid Neural Network (CSMSELNN) version 2
 # python csmselnn_mnist_v2.py
-# Test Accuracy: 98.26%
+# Test Accuracy: 98.74%
